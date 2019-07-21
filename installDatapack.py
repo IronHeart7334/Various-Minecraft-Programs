9f9314dc8ed04892b3358dbf48d45f2a8050eb2a
IronHeart7334 <greengrappler12@gmail.com>
2019-07-21 16:09:50 -0700

    Wow. That was easier to write than I thought
    totally want to test this ASAP

---
+++ installDatapack.py
@@ -0,0 +1,27 @@
+from os import listdir
+from os.path import isdir
+from shutil import copyfile, copytree
+
+done = False
+ip = ""
+packs = listdir("./datapacks")
+target = ""
+
+print("Welcome to Matt's Minecraft Datapack install utility")
+while not done:
+    print("Enter the full path to the directory where you wish to install datapacks")
+    #todo: just enter the world name, this finds the proper directory
+    print("(minecraft/saves/WORLD/datapacks)")
+    while not isdir(target):
+        target = input("Enter path: ")
+    
+    print("Available datapacks:")
+    for datapack in packs:
+        print("* " + datapack)
+    ip = input("Select a datapack to install in this world, or type 'exit' to quit: ")
+    if ip == "exit":
+        break;
+    if ip in packs:
+        copytree("./datapacks/" + ip, target + "/" + ip)
+    else:
+        print("'" + ip + "\' is not in the list of datapacks")
